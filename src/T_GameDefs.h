97dd54a3c361ac5e994b0ed253bd9e99c89789d6
pawel <plapacz6@gmail.com>
2024-03-07 16:54:26 +0100

    place of speed def

--- src/T_GameDefs.h
+++ src/T_GameDefs.h
@@ -19,18 +19,26 @@ class T_GameDefs {
     int road_hight;  //length of the visible part of road
     int board_width;  //== ( roadside_width * 2 + lane_width * 3 ),
     int steppebush_max_distance; // = (board_width - lane_width),
+    int horizontal_step;  /**< distances tarveled horizontaly by bush in one unit time*/
+
     std::string wndName;
+
     long time_period_ns;
     time_t time_period_s;
     long time_speed_up_ns;
     time_t time_speed_up_s;
-    
+                    
     double const car1_step_speed_up;
-    
-    int horizontal_step;  /*< distances tarveled horizontaly by bush in one unit time*/
+    double const car_max_speed;        /**< max speed of each car*/
+    double const car_beginnig_speed;   /**< beginnig speed of each car*/
 
-    double const car_max_speed;       /*< max speed of each car*/
-    double const car_beginnig_speed;   /*< beginnig speed of each car*/
+    //TODO  - calculate in compile time : //ARYTMINT
+    /* //TODO: Internal unit of speed:   mm/sec  ???   mm/one_frame_period ?
+    shirt speed to mm allow perhaps to move to int aritmetic for speed
+    and perhaps bitshifting by <<6 may be similiar to *1000 and further back to px >>6
+    */
+    // double const Icar_max_speed;       /**< car_max_speed in game Interal units*/
+    // double const Icar_beginnig_speed;  /**< car_beginnig_speed in game Internal units*/
 
     /**
      * @brief calculate coordinates of rectangle where object image
